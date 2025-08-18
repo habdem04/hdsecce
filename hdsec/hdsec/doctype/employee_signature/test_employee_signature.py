d32eaa96e6849ac43cca88e8fa74cf370dd91ef0
habdem04 <habdem2003@gmail.com>
2025-08-18 22:12:06 +0300

    employee_signature

---
+++ hdsec/hdsec/doctype/employee_signature/test_employee_signature.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, HD and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmployeeSignature(FrappeTestCase):
+	pass
